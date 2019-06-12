3eb7c72a48a35eabfc06f471d443e64dac124b76
Krzysztof Płaczek <krzysztof.placzek@yahoo.com>
2019-06-12 21:18:00 +0200

    Introduced Providers and command line interface to choose them; Added Wikipedia provider; Added public method getEmojis

--- src/ixrk/Main.java
+++ src/ixrk/Main.java
@@ -1,30 +1,58 @@
 package ixrk;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
-
-    private static final String EMOJI_URL = "https://unicode.org/Public/emoji/latest/emoji-test.txt";
-
     public static void main(String[] args) {
         try {
-            List<Emoji> emojis = getEmojiList();
+            List<Emoji> emojis = getEmojis(parseProvider(args));
             emojis.forEach(System.out::println);
         } catch (IOException e) {
             e.printStackTrace();
-            System.exit(3);
+            System.exit(2);
         }
     }
 
-    private static List<Emoji> getEmojiList() throws IOException {
-        URL url = new URL(EMOJI_URL);
+    public static List<Emoji> getEmojis(Provider provider) throws IOException {
+        if (provider == null) {
+            return getUnicodeTestEmojis();
+        }
+        switch (provider) {
+            case WIKIPEDIA:
+                return getWikipediaEmojis();
+            case UNICODE_EMOJI_TEST:
+            default:
+                return getUnicodeTestEmojis();
+        }
+    }
+
+    private static Provider parseProvider(String[] args) {
+        if (args.length > 0) {
+            switch (args[0]) {
+                case "wikipedia":
+                    return Provider.WIKIPEDIA;
+                case "unicode-test":
+                    return Provider.UNICODE_EMOJI_TEST;
+                default:
+                    return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    private static List<Emoji> getUnicodeTestEmojis() throws IOException {
+        URL url = new URL(Provider.UNICODE_EMOJI_TEST.getUrl());
         BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
         List<String> lines = reader.lines()
                 .filter(v -> v.contains("; fully-qualified"))
@@ -41,4 +69,24 @@ public class Main {
         }
         return emojis;
     }
+
+    private static List<Emoji> getWikipediaEmojis() throws IOException {
+        Document doc = Jsoup.connect(Provider.WIKIPEDIA.getUrl()).get();
+        Element tableBody = doc.getElementsByClass("wikitable nounderlines")
+                .last()
+                .getElementsByTag("tbody")
+                .first();
+
+        List<Emoji> emojis = new ArrayList<>();
+        for (Element td : tableBody.getElementsByTag("td")) {
+            if (td.hasAttr("title") && td.attr("title").matches("^U\\+.*: .+")) {
+                String emojiInfo = td.attr("title");
+                String emojiValue = td.getElementsByTag("a").text();
+                String emojiDescription = emojiInfo.substring(2 + emojiInfo.indexOf(":"));
+                emojis.add(new Emoji(emojiValue, emojiDescription));
+            }
+        }
+        return emojis;
+    }
 }
+
